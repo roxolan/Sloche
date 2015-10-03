f99f1647686612d30b625f3ef0dda3d1b060bcb4
Victor <roxolanus@gmail.com>
2015-10-03 14:36:02 +0300

    changing it to Babel for index.html

--- Sloche/Scripts/App.js
+++ Sloche/Scripts/App.js
@@ -18,30 +18,30 @@ var user = context.get_web().get_currentUser();
 
 // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
 $(document).ready(function () {
-  getUserName();
+  // getUserName();
 });
 
 // This function prepares, loads, and then executes a SharePoint query to get the current users information
-function getUserName() {
-  context.load(user);
-  context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
-}
+//function getUserName() {
+//  context.load(user);
+//  context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//}
 
 // This function is executed if the above call is successful
 // It replaces the contents of the 'message' element with the user name
-function onGetUserNameSuccess() {
-  $('#message').text('Hello ' + user.get_title());
-  $('#message').append('<br/>ReactJS is around!');
-  var HelloPrinting = React.createClass({
-    render: function () {
-      return(
-        React.createElement("div", {className: "container"}),
-          React.createElement("h1", null, "Getting Started...")
-      );
-    }
-  });
-  React.render(React.createElement(HelloPrinting, null), message);
-}
+//function onGetUserNameSuccess() {
+//  $('#message').text('Hello ' + user.get_title());
+//  $('#message').append('<br/>ReactJS is around!');
+//  var HelloPrinting = React.createClass({
+//    render: function () {
+//      return(
+//        React.createElement("div", {className: "container"}),
+//          React.createElement("h1", null, "Getting Started...")
+//      );
+//    }
+//  });
+//  React.render(React.createElement(HelloPrinting, null), message);
+//}
 
 // This function is executed if the above call fails
 function onGetUserNameFail(sender, args) {
